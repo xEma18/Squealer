e81a233f51f0fe6a981989823d4d3c9d186eee9c
manuel091002 <manuelflagelli02@gmail.com>
2023-10-16 19:59:23 +0200

    implementata conversione base64 per immagine profilo

--- App/client/src/SignUp3.jsx
+++ App/client/src/SignUp3.jsx
@@ -6,6 +6,27 @@ const SignUp3=({updateRegistrationData})=>{
     const navigate = useNavigate();
     const [image, setImage]=useState('');
 
+    //funzione copiata che mi converte un file in base64 in modo  da poter salvare l'immagine nel db come stringa
+    const convertToBase64 = (file) => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          resolve(reader.result);
+        };
+        reader.onerror = reject;
+        reader.readAsDataURL(file);
+      });
+    };
+
+    const handleImageChange = async (e) => {
+      const file = e.target.files[0]; //prendo il primo file selezionato (siccome potrei selezionare più file)
+  
+      if (file) { //controlla che il file aggiunto non sia vuoto
+        const base64Image = await convertToBase64(file);
+        setImage(base64Image);
+      }
+    };
+
     const handleNext=async ()=>{
         updateRegistrationData({
           image,
@@ -24,6 +45,9 @@ const SignUp3=({updateRegistrationData})=>{
             <form id="signup-form">
             <div className="title t-small">Scegli un immagine profilo</div>
             <div className="subtitle">Hai un'immagine che ti rappresenta? Caricala.</div>
+            <input type="file" accept="image/*" onChange={handleImageChange}  />
+            {/* Visualizzare l'immagine selezionata */}
+            {image && <img src={image} alt="Immagine profilo" style={{ maxWidth: '100%', maxHeight: '200px' }} />}
             <div className="btn btn-avanti" id="btn-signup-2" onClick={handleNext}>Avanti</div>
             </form>
         </>
