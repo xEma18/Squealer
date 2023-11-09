8bbd79080525e29663934116cde3cf6d5a5f135d
jacopoamoretti <98223896+jacopoamoretti@users.noreply.github.com>
2023-11-09 21:39:00 +0100

    Aggiunte

--- Moderator Dashboard/script.js
+++ Moderator Dashboard/script.js
@@ -5,38 +5,59 @@ function cambiaPagina(url) {
 
 /*Gestione modifica e salvataggio parametri utente*/
 
+// Prendo il box che contiene i bottoni
+const parentElement = document.getElementById("BtnBox");
+
 // Ottieni il riferimento al pulsante "Modifica" e ai campi dati
+/*
 const modificaBtn = document.getElementById("modificaBtn");
 const tipoUtenteField = document.querySelector("h6:nth-child(3)");
 const popolaritaField = document.querySelector("h6:nth-child(4)");
 const caratteriGiornalieriField = document.querySelector("h6:nth-child(5)");
 const caratteriSettimanaliField = document.querySelector("h6:nth-child(6)");
 const caratteriMensiliField = document.querySelector("h6:nth-child(7)");
+*/
 
-const utenteModificato = {
-    tipoUtente: tipoUtenteField.textContent,
-    popolarita: popolaritaField.textContent,
-    caratteriGiornalieri: parseInt(caratteriGiornalieriField.textContent),
-    caratteriSettimanali: parseInt(caratteriSettimanaliField.textContent),
-    caratteriMensili: parseInt(caratteriMensiliField.textContent)
-};
+
 
 // Aggiungi un gestore di eventi al pulsante "Modifica"
-modificaBtn.addEventListener("click", function () {
+function ModifyButton(cardId) {
+    // Capisco che card è stata selezionata
+    const card = document.getElementById(cardId);
+
+    // Prendo tutti i campi della card
+    const modificaBtn = card.querySelector("#modificaBtn");
+    const tipoUtenteField = card.querySelector('h6:nth-child(3)');
+    const popolaritaField = card.querySelector('h6:nth-child(4)');
+    const caratteriGiornalieriField = document.querySelector("h6:nth-child(5)");
+    const caratteriSettimanaliField = document.querySelector("h6:nth-child(6)");
+    const caratteriMensiliField = document.querySelector("h6:nth-child(7)");
+
     // Rendi invisibile il pulsante modifica
     modificaBtn.style.display = "none";
 
     // Abilita la modifica dei campi
-    tipoUtenteField.innerHTML = `<span class="fw-bold">Tipo utente:</span><select id="tipoUtenteInput">
+    tipoUtenteField.innerHTML = `<span class="fw-bold">Tipo utente:</span>
+    <select id="tipoUtenteInput">
         <option value="VIP">VIP</option>
         <option value="Normale">Normale</option>
         <option value="Premium">Premium</option>
     </select>`;
-    popolaritaField.innerHTML = `<span class="fw-bold">Popolarità:</span> <select id="popolaritaInput">
+    popolaritaField.innerHTML = `<span class="fw-bold">Popolarità:</span>
+    <select id="popolaritaInput">
         <option value="Alta">Alta</option>
         <option value="Media">Media</option>
         <option value="Bassa">Bassa</option>
     </select>`;
+
+    const utenteModificato = {
+        tipoUtente: tipoUtenteField.textContent,
+        popolarita: popolaritaField.textContent,
+        caratteriGiornalieri: parseInt(caratteriGiornalieriField.textContent),
+        caratteriSettimanali: parseInt(caratteriSettimanaliField.textContent),
+        caratteriMensili: parseInt(caratteriMensiliField.textContent)
+    };
+
     caratteriGiornalieriField.innerHTML = `<span class="fw-bold">Caratteri Giornalieri:</span> <input type="number" id="caratteriGiornalieriInput" value="${utenteModificato.caratteriGiornalieri}">`;
     caratteriSettimanaliField.innerHTML = `<span class="fw-bold">Caratteri Settimanali:</span> <input type="number" id="caratteriSettimanaliInput" value="${utenteModificato.caratteriSettimanali}">`;
     caratteriMensiliField.innerHTML = `<span class="fw-bold">Caratteri Mensili:</span> <input type="number" id="caratteriMensiliInput" value="${utenteModificato.caratteriMensili}">`;
@@ -78,4 +99,22 @@ modificaBtn.addEventListener("click", function () {
         // Mostra nuovamente il pulsante "Modifica"
         modificaBtn.style.display = "inline-block";
     });
+};
+
+
+parentElement.addEventListener("click", function(event) {
+    if (event.target.id == "modificaBtn") {
+        console.log(event)
+        // Verifica se l'elemento di destinazione del clic è un pulsante con l'id "modificaBtn"
+        const card = event.target.closest(".card"); // Trova la card padre dell'elemento cliccato
+
+        if (card) {
+            console.log(card)
+            // Ottieni l'id univoco assegnato direttamente alla card
+            const cardId = card.id;
+            console.log(cardId)
+            // Chiama la funzione di modifica specifica per la card corrispondente
+            ModifyButton(cardId);
+        }
+    }
 });
